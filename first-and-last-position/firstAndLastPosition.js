0af0353a3de9284adcf501540b1b5abf3af6c502
Lena Hou <lenahou2013@gmail.com>
2022-03-11 19:30:53 -0500

    adding find first and last position problem

---
+++ first-and-last-position/firstAndLastPosition.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------sudo code-----------------------------------
+//   def search(arr, target):
+//      left = 0 
+//      right = arr.length -1
+//      while left <= right:
+//      mid = (left + right) / 2
+//        if arr[mid] == target:
+//            return mid
+//         else if target < arr[mid]:
+//            right = mid - 1
+//         else:
+//            left = mid + 1
+//    return -1
+// ----------------------------------------------------------------------     
